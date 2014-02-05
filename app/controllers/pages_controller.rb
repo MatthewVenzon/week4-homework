ade4669352a1935a631f6e756e5cb45ac3b4fe23
MatthewVenzon <matthew.venzon@gmail.com>
2014-02-05 11:06:13 -0600

    Completed HW4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,23 @@
-  class PagesController < ApplicationController
+
+class PagesController < ApplicationController
+
+  #The "/Index" page shows neighborhoods people can learn about.
+  #The Greenwich Village link will take you to the "/show" page
+
+  def index
+    @mnneighborhood_list = ["Greenwich Village", "Financial District", "Seaport", "Tribeca", "Soho", "Chinatown", "Nolita", "East Village",
+      "Union Square", "Grammercy", "Flatiron", "Murray Hill", "Midtown East", "Midtown West", "Hell's Kitchen",
+      "Columbus Circle", "Upper West Side", "Upper East Side", "Morningside Heights", "Harlem", "Spanish Harlem", "Sugar Hill",
+      "Washington Heights", "Inwood"]
   end
+
+  def show
+    @mnneighborhood = "Greenwich Village"
+  end
+
+end
+
+    # @bkneighborhood_list = ["Brooklyn Heights", "Dumbo", "Downtown Brooklyn", "Ft. Greene", "Carroll Gardens", "Cobble Hill", "Boerum Hill",
+    #   "Navy Yard", "Vinegar Hill", "Park Slope", "Ditmas Park", "Flatbush", "Sunset Park", "Red Hook", "Clinton Hill,",
+    #   "Lefferts Gardens", "Prospect Heights", "Crown Heights", "East New York", "Brownsville", "Bushwick", "Williamsburg",
+    #   "Greenpoint", "Coney Island", "Brighton Beach", "Bensonhurst", "Bay Ridge"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Week4hw::Application.routes.draw do
 
   get "/" => "pages#home"
+  get "/index" => "pages#index"
+  get "/show" => "pages#show"
 
-  # HINT: Add more routes here...
+  get "/weather/search" => "weather#search"
+  get "/weather/conditions" => "weather#conditions"
+
 
 
 
